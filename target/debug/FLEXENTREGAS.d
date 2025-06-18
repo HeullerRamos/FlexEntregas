97feae53a7bce08188641320980712f7c69da427
root <root@DESKTOP-A0FL8MG.localdomain>
2025-06-18 08:29:53 -0300

    commit inicial

---
+++ target/debug/FLEXENTREGAS.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/heull/Desktop/FlexEntregas/target/debug/FLEXENTREGAS: /mnt/c/Users/heull/Desktop/FlexEntregas/src/main.rs
---
+++ target/debug/deps/FLEXENTREGAS-759d4ec3d23892ce.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/heull/Desktop/FlexEntregas/target/debug/deps/FLEXENTREGAS-759d4ec3d23892ce: src/main.rs
+
+/mnt/c/Users/heull/Desktop/FlexEntregas/target/debug/deps/FLEXENTREGAS-759d4ec3d23892ce.d: src/main.rs
+
+src/main.rs:
